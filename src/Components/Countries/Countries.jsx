da00f422e15eed9dc838e765ef5b239358748b55
TazwoarCommits <tazwoarlives@gmail.com>
2024-10-20 20:33:13 +0600

    worked on hanfler

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,22 +1,31 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../country/Country";
+import './Countries.css'
 
 
 const Countries = () => {
 
-    const [countries , setCountries] = useState([]) ;
+    const [countries, setCountries] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
 
     return (
         <div>
             <h1>Countries : {countries.length}</h1>
-            
+            <div>
+                <h2>Visited Countries</h2>
+            </div>
+            <div className="country-countainer">
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+
+            </div>
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,11 +1,23 @@
-import '../Country/Country.css'
+import { useState } from 'react';
+import './Country.css'
 const Country = ({country}) => {
-    const {name , flags} = country
-    console.log(country);
+    const {name , flags ,population , area , cca3 } = country
+    const [visited , setState] = useState(false)
+
+    const handleVisitState = () =>{
+        setState(!visited) ;
+    }
+
+    console.log(country)
     return (
-        <div className='country'>
-            <h3>Name : {name?.common} </h3>
-            <img src={flags.png} alt="" />
+        <div className={`country ${visited? 'visited' : 'default'}`}>
+            <h3>Name : {name?.common}</h3>
+            <img src={flags['png']} alt="" />
+            <h4> Population : {population}</h4>
+            <h4> Area : {area}</h4>
+            <p>cca3 : {cca3}</p>
+            <button className={visited && 'visit-btn'} onClick={handleVisitState}>{visited? 'visited' : 'visit'}</button>
+            {/* {visited ?' i have visited'  : ' want to go'} */}
         </div>
     );
 };
